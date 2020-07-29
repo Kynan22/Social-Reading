ac5871dadc17bbf2f016cc60691a1ed974c9e6f3
Kynan Brown <31174798+Kynan22@users.noreply.github.com>
2020-07-29 18:01:56 +1000

    update shelf widgets

--- lib/screens/reading/components/widgets/shelf_widgets.dart
+++ lib/screens/reading/components/widgets/shelf_widgets.dart
@@ -1,3 +1,4 @@
+import 'package:book_app/models/database/shelf_db.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:book_app/classes/book.dart';
@@ -7,47 +8,55 @@ import 'package:book_app/models/database.dart';
 import 'package:book_app/models/api.dart';
 
 class ShelfWidgets {
-  Future<Widget> shelf(context, shelf) async {
-    //Book books = await Api().fetchBooks(value);
 
-    List<Container> shelfList=[];
-    // Book books = await
-    // for(var item in books.items){
-    //   shelfList.add(Container(
-    //     color: lightGrey,
-    //     child: Row(
-    //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-    //       children: <Widget>[
-    //         new Container(
-    //           height: 120,
-    //           width: 100,
-    //           padding: EdgeInsets.only(top: 5, bottom: 5),
-    //           child: Image.network(item.volumeInfo.imageLinks.toJson()['thumbnail'].toString(), fit: BoxFit.fitHeight,),
-    //         ),
-    //         //onTap: () => Database().addBook(context, isbn[x])),
+  
 
-    //         new Column(
-    //           mainAxisAlignment: MainAxisAlignment.start,
-    //           crossAxisAlignment: CrossAxisAlignment.start,
-    //           children: <Widget>[
-    //             Text(item.volumeInfo.toJson()['title'].toString(), style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
-    //             Text(item.volumeInfo.toJson()['authors'][0].toString(), style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400)),
-    //             Text("Published " + item.volumeInfo.toJson()['publishedDate'].toString(), style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400)),
-    //           ],
-    //         ),
-    //         new GestureDetector(
-    //           child: Icon(
-    //             Icons.add_circle,
-    //             color: darkGrey,
-    //             size: 30,
-    //           ),
-    //           onTap: () => Database().checkBook(context, item.volumeInfo.industryIdentifiers[0].toJson()['identifier'].toString(), shelf),
-    //         ),
-    //       ],
-    //     ),
-    //   ));
-    // }
+
+  // Future<Widget> shelf(context, shelf) async {
+  //   //Book books = await Api().fetchBooks(value);
+
+  //   List<Container> shelfList=[];
+  //   Book books = await 
+  //   for(var item in books.items){
+  //     shelfList.add(Container(
+  //       color: lightGrey,
+  //       child: Row(
+  //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //         children: <Widget>[
+  //           new Container(
+  //             height: 120,
+  //             width: 100,
+  //             padding: EdgeInsets.only(top: 5, bottom: 5),
+  //             child: Image.network(item.volumeInfo.imageLinks.toJson()['thumbnail'].toString(), fit: BoxFit.fitHeight,),
+  //           ),
+  //           //onTap: () => Database().addBook(context, isbn[x])),
+
+  //           new Column(
+  //             mainAxisAlignment: MainAxisAlignment.start,
+  //             crossAxisAlignment: CrossAxisAlignment.start,
+  //             children: <Widget>[
+  //               Text(item.volumeInfo.toJson()['title'].toString(), style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+  //               Text(item.volumeInfo.toJson()['authors'][0].toString(), style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400)),
+  //               Text("Published " + item.volumeInfo.toJson()['publishedDate'].toString(), style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400)),
+  //             ],
+  //           ),
+  //           new GestureDetector(
+  //             child: Icon(
+  //               Icons.add_circle,
+  //               color: darkGrey,
+  //               size: 30,
+  //             ),
+  //             onTap: () => Database().checkBook(context, item.volumeInfo.industryIdentifiers[0].toJson()['identifier'].toString(), shelf),
+  //           ),
+  //         ],
+  //       ),
+  //     ));
+  //   }
     
     
+  // }
+
+  shelfOpen(context, shelf) async{
+    return ShelfDB().openShelf(shelf);
   }
 }
